42b64879d431c67a310d2ddbac01f9fbe6a409ba
willy9461 <guillermogalarraga1986@gmail.com git config --global user.email guillermogalarraga1986@gmail.com>
2024-08-10 16:22:48 -0300

    first commit

---
+++ src/hooks/useOrder.ts
@@ -0,0 +1,14 @@
+import { useState } from "react"
+import type { MenuItem, OrderItem } from "../types"
+
+export default function useOrder() {
+    const [order, setOrder] = useState<OrderItem[]>([])
+    const addItem = (item : MenuItem) => {
+        console.log(item);
+    }
+
+    
+    return {
+        addItem 
+    }
+}
